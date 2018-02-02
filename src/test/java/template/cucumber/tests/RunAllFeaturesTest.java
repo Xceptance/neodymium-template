60d19614629823aff3eb4144dc5b01f79aaf79f6
Marcus Kaufmann <m.kaufmann@xceptance.net>
2018-02-02 16:10:21 +0100

    changed runner to NeodymiumCucumberRunner

--- src/test/java/template/cucumber/tests/RunAllFeaturesTest.java
+++ src/test/java/template/cucumber/tests/RunAllFeaturesTest.java
@@ -2,15 +2,16 @@ package template.cucumber.tests;
 
 import org.junit.runner.RunWith;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import com.xceptance.neodymium.NeodymiumCucumberRunner;
 
-@RunWith(Cucumber.class)
+import cucumber.api.CucumberOptions;
+
+@RunWith(NeodymiumCucumberRunner.class)
 @CucumberOptions(features = "src/test/java/template/cucumber/features", glue = "template", monochrome = true, plugin =
-{
-  "pretty", // console output
-  "html:target/cucumber-report/", // html report
-})
+    {
+        "pretty", // console output
+        "html:target/cucumber-report/", // html report
+    })
 public class RunAllFeaturesTest
 {
 }
