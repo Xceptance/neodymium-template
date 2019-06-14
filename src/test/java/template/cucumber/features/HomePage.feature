9c8bded6bdd67e9e1b8232c1b7b0a3f6d6de76a3
Marcel Pfotenhauer <m.pfotenhauer@xceptance.net>
2019-06-14 10:17:57 +0200

    fixed typo

--- src/test/java/template/cucumber/features/HomePage.feature
+++ src/test/java/template/cucumber/features/HomePage.feature
@@ -3,7 +3,7 @@ Feature: Home page
 
   Scenario Outline: Visting the home page
     Given The browser "<browser>" is open
-    Then I navigate to he home page of the project
+    Then I navigate to the home page of the project
     
     Then The home page should have heading, carousel, services and the company button
     And The page title should be "Xceptance - The Software Testing Experts"
--- src/test/java/template/flows/OpenHomePageFlow.java
+++ src/test/java/template/flows/OpenHomePageFlow.java
@@ -1,16 +1,16 @@
 package template.flows;
 
+import com.xceptance.neodymium.util.Neodymium;
+
 import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 import static com.codeborne.selenide.Selenide.open;
-
-import com.xceptance.neodymium.util.Neodymium;
 
 import cucumber.api.java.en.And;
 import template.pageobjects.pages.HomePage;
 
 public class OpenHomePageFlow
 {
-    @And("^I navigate to he home page of the project$")
+    @And("^I navigate to the home page of the project$")
     public static HomePage flow()
     {
         // clear browser cookies to remove old data
