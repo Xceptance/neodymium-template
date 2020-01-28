7f1913830f641187b6f24dc0dbff38adfaec620d
Johannes Sengbusch <j.sengbusch@xceptance.com>
2020-01-28 11:35:56 +0100

    first try to run test with applitools

--- src/test/java/template/neodymium/tests/smoke/HomePageTest.java
+++ src/test/java/template/neodymium/tests/smoke/HomePageTest.java
@@ -1,7 +1,11 @@
 package template.neodymium.tests.smoke;
 
 import org.junit.Test;
+import org.junit.runners.model.InitializationError;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 
+import com.applitools.eyes.selenium.Eyes;
 import com.xceptance.neodymium.util.Neodymium;
 
 import template.flows.OpenHomePageFlow;
@@ -10,16 +14,28 @@ import template.pageobjects.pages.HomePage;
 
 public class HomePageTest extends AbstractTest
 {
+
     @Test
-    public void testVisitingHomepage()
+    public void testVisitingHomepage() throws InitializationError
     {
+        Eyes eyes = new Eyes();
+
+        String apiKey = "";
+        eyes.setApiKey(apiKey);
         // Goto the home page
+
         HomePage homePage = OpenHomePageFlow.flow();
+        EventFiringWebDriver eventFiringWebDriver = (EventFiringWebDriver) Neodymium.getDriver();
+        RemoteWebDriver remoteWebDriver = (RemoteWebDriver) eventFiringWebDriver.getWrappedDriver();
+        eyes.open(remoteWebDriver, "HomePage", "HomePageTest");
 
         // short validation to check that the correct page was opened, should be moved to OpenHomePageFlow
         homePage.isExpectedPage();
         // basic validation
         homePage.validateStructure();
         homePage.title.validateTitle(Neodymium.localizedText("homepage.title"));
+
+        eyes.checkWindow("HomePage Window");
+        eyes.closeAsync();
     }
 }
