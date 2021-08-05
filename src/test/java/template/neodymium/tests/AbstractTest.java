0054a1ab248f3bd5a739e62bc1da78868970f547
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2021-08-05 09:51:25 +0200

    use Chrome_1400x1000

--- src/test/java/template/neodymium/tests/AbstractTest.java
+++ src/test/java/template/neodymium/tests/AbstractTest.java
@@ -9,7 +9,7 @@ import com.xceptance.neodymium.module.statement.browser.multibrowser.Browser;
  * @author pfotenhauer
  */
 @RunWith(NeodymiumRunner.class)
-@Browser("Chrome_1024x768")
+@Browser("Chrome_1400x1000")
 public abstract class AbstractTest
 {
 }
