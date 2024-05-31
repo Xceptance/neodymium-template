572c4609d4d19a16aa9428d7698a3f565705dc2b
Olha Omelianchuk <o.omelianchuk@xceptance.net>
2024-05-31 17:17:18 +0200

    remove NeodymiumRunner as not needed

--- src/test/java/template/neodymium/tests/AbstractTest.java
+++ src/test/java/template/neodymium/tests/AbstractTest.java
@@ -1,14 +1,10 @@
 package template.neodymium.tests;
 
-import org.junit.runner.RunWith;
-
 import com.xceptance.neodymium.common.browser.Browser;
-import com.xceptance.neodymium.junit4.NeodymiumRunner;
 
 /**
  * @author pfotenhauer
  */
-@RunWith(NeodymiumRunner.class)
 @Browser("Chrome_1400x1000")
 public abstract class AbstractTest
 {
