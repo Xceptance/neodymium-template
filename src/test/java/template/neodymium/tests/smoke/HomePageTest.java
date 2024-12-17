ec4900d1e60956d7b2d4ee3c8b6ed2c77789bf0f
RobertAvemarg <jan.robert.avemarg@gmail.com>
2024-12-17 16:06:18 +0100

    initial test - seems to work

--- src/test/java/template/neodymium/tests/smoke/HomePageTest.java
+++ src/test/java/template/neodymium/tests/smoke/HomePageTest.java
@@ -2,10 +2,13 @@ package template.neodymium.tests.smoke;
 
 import com.xceptance.neodymium.junit5.NeodymiumTest;
 import com.xceptance.neodymium.util.Neodymium;
-
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import template.flows.OpenPageFlows;
 import template.neodymium.tests.AbstractTest;
 
+@ExtendWith(MyTestListener.class)
 public class HomePageTest extends AbstractTest
 {
     @NeodymiumTest
@@ -17,5 +20,30 @@ public class HomePageTest extends AbstractTest
         // basic validation
         homePage.validateStructure();
         homePage.title.validateTitle(Neodymium.localizedText("homepage.title"));
+    }
+}
+
+class MyTestListener implements AfterTestExecutionCallback
+{
+    @Override
+    public void afterTestExecution(ExtensionContext extensionContext) throws Exception
+    {
+        System.out.println("afterTestExecution");
+
+        // Get the test method name
+        String testMethodName = extensionContext.getRequiredTestMethod().getName();
+        System.out.println(testMethodName);
+        System.out.println(extensionContext.getTestClass());
+        System.out.println(extensionContext.getTestInstance());
+
+        // Find the Allure result file (adjust the filename pattern as needed)
+        // ... (Code to find the Allure result file as shown in the previous example) ...
+
+        // Process the test result (e.g., read the result file, extract data)
+        // ... (Your logic to process the result file) ...
+
+        // Send the processed data to the server
+        // ... (Your logic to send data to the server) ...
+
     }
 }
